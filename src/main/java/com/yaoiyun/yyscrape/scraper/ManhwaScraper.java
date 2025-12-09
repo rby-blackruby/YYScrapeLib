8cac30a01bff70d440e02deefcc84a7cc0866bf4
blackruby <ruby@blackruby.hu>
2025-12-10 00:13:33 +0100

    Added ManhwaScraper specific Builder implementation and test field

--- src/main/java/com/yaoiyun/yyscrape/scraper/ManhwaScraper.java
+++ src/main/java/com/yaoiyun/yyscrape/scraper/ManhwaScraper.java
@@ -1,11 +1,28 @@
 package com.yaoiyun.yyscrape.scraper;
 
 public class ManhwaScraper extends AbstractScraper implements Scraper {
+    private final String manhwaScraperSpecificStringExample;
 
-    private ManhwaScraper() {
-        super();
+    private ManhwaScraper(Builder builder) {
+        super(builder);
+        manhwaScraperSpecificStringExample = builder.manhwaScraperSpecificStringExample;
     }
 
-    
+    public String getManhwaScraperSpecificStringExample() {
+        return manhwaScraperSpecificStringExample;
+    }
 
+    public static class Builder extends AbstractScraper.Builder<Builder> {
+        private String manhwaScraperSpecificStringExample;
+
+        public Builder setManhwaScraperSpecificExample(String value) {
+            this.manhwaScraperSpecificStringExample = value;
+            return this;
+        }
+
+        @Override
+        public ManhwaScraper build() {
+            return new ManhwaScraper(this);
+        }
+    }
 }
