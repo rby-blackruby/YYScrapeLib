28505ecef4311fe3ba8366cd1c29775a83ce0128
blackruby <ruby@blackruby.hu>
2025-12-11 22:11:21 +0100

    Added missing parameter

--- src/main/java/com/yaoiyun/yyscrape/utils/UrlUtils.java
+++ src/main/java/com/yaoiyun/yyscrape/utils/UrlUtils.java
@@ -3,7 +3,7 @@ package com.yaoiyun.yyscrape.utils;
 public class UrlUtils {
 
     // TODO split the url with and get the [1] element, that should be the xyz.com base url part.
-    public static String getBaseUrl() {
+    public static String getBaseUrl(String url) {
         return "";
     }
 
