62fa74ef8a8dbcac8732a2df4fb299e6ac09e885
blackruby <ruby@blackruby.hu>
2025-12-14 03:02:20 +0100

    added array index out of bounds check

--- src/main/java/com/yaoiyun/yyscrape/utils/UrlUtils.java
+++ src/main/java/com/yaoiyun/yyscrape/utils/UrlUtils.java
@@ -6,8 +6,14 @@ public class UrlUtils {
 
     // TODO split the url with and get the [1] element, that should be the xyz.com base url part.
     // urlArr is: [https:, , mangabuddy.com] -- actually needs array[2]
+    // this is going to give an out of bounds error if the url is not formatted correctly
     public static String getBaseUrl(String url) {
-        String[] urlArr = url.split("//");
+        String[] urlArr = url.split("/");
+
+        if(urlArr.length < 3) {
+            throw new IllegalArgumentException("Url with incorrect formatting cannot be split.");
+        }
+
         return urlArr[2];
     }
 
