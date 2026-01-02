119349d9fa530356f4f48620c0e9ee7001997c3a
blackruby <ruby@blackruby.hu>
2026-01-02 22:36:30 +0100

    now uses separate scraper supplier

--- src/main/java/com/yaoiyun/yyscrape/scraper/ContentScraperFactory.java
+++ src/main/java/com/yaoiyun/yyscrape/scraper/ContentScraperFactory.java
@@ -5,15 +5,14 @@ import com.yaoiyun.yyscrape.utils.UrlUtils;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Supplier;
 
 public final class ContentScraperFactory {
-    private static final Map<String, ScraperSupplier<? extends ContentScraper>> CONTENT_SCRAPER_REGISTRY = new ConcurrentHashMap<>();
+    private static final Map<String, ScraperSupplier<ContentScraper>> CONTENT_SCRAPER_REGISTRY = new ConcurrentHashMap<>();
 
     private ContentScraperFactory() {
     }
 
-    public static void register(String websiteBaseUrl, ScraperSupplier<? extends ContentScraper> contentScraperClass) {
+    public static void register(String websiteBaseUrl, ScraperSupplier<ContentScraper> contentScraperClass) {
         CONTENT_SCRAPER_REGISTRY.putIfAbsent(websiteBaseUrl, contentScraperClass);
     }
 
