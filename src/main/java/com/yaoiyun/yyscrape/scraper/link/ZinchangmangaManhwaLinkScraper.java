e9c46daab5ce226dd18a72b8d53ceb83cfa0a01e
blackruby <ruby@blackruby.hu>
2026-01-02 03:13:19 +0100

    overwrites parent post load action function instead of the entire discover content urls function

--- src/main/java/com/yaoiyun/yyscrape/scraper/link/ZinchangmangaManhwaLinkScraper.java
+++ src/main/java/com/yaoiyun/yyscrape/scraper/link/ZinchangmangaManhwaLinkScraper.java
@@ -1,13 +1,11 @@
 package com.yaoiyun.yyscrape.scraper.link;
 
 import com.yaoiyun.yyscrape.content.ScrapableContent;
-import com.yaoiyun.yyscrape.scraper.exception.CloudflareBlockedException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.time.Duration;
-import java.util.List;
 
 public class ZinchangmangaManhwaLinkScraper extends ManhwaLinkScraper {
     public ZinchangmangaManhwaLinkScraper(WebDriver webDriver, ScrapableContent assignedContent) {
@@ -15,24 +13,9 @@ public class ZinchangmangaManhwaLinkScraper extends ManhwaLinkScraper {
     }
 
     @Override
-    public List<String> discoverContentUrls() {
-        this.getWebDriver().get(this.getAssignedContent().getUrl());
-        // TODO: use explicit wait instead of implicit x seconds
-        this.getWebDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
-        // an entirely new class was required just for clicking this button, maybe do
-        // something in the ManhwaLinkScraper class instead?
+    protected void postLoadAction() {
         WebElement showMoreButton = this.getWebDriver().findElement(By.className("chapter-readmore"));
         showMoreButton.click();
         this.getWebDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
-        List<WebElement> elements = this.findWebsiteElementsByCssSelector("a");
-        for(var element : elements) {
-            if(element.getText().contains("Cloudflare")) {
-                throw new CloudflareBlockedException("Manhwa link scraper blocked by Cloudflare. URL: " + this.getAssignedContent().getUrl());
-            }
-        }
-
-        return getFilteredChapterUrls(elements);
     }
 }
