12f91794d0736b1924260d88844c97b732c99f83
blackruby <ruby@blackruby.hu>
2026-01-06 19:48:25 +0100

    added exception to mark unimplemented but planned scraper logic

---
+++ src/main/java/com/yaoiyun/yyscrape/scraper/exception/NotImplementedException.java
@@ -0,0 +1,7 @@
+package com.yaoiyun.yyscrape.scraper.exception;
+
+public class NotImplementedException extends RuntimeException {
+    public NotImplementedException(String message) {
+        super(message);
+    }
+}
